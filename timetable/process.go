f4b95f9848029f65ebcc1f3df98b090afec74ef3
Peter Mount <peter@retep.org>
2018-03-21 19:37:12 +0000

    Scanning schedules to build index working

--- timetable/process.go
+++ timetable/process.go
@@ -10,9 +10,20 @@ func (i *index) process( k, b []byte ) error {
   dec := codec.NewBinaryCodecFrom( b )
   dec.Read( s )
 
-//  for _, l := range s.Locations {
-//
-//  }
+  // Don't use Cancellations but TODO check we handle these in final results
+  if s.ID.STPIndicator != "C" {
 
+    key := s.Key()
+
+    // Add locations that are stations to the map
+    for _, l := range s.Locations {
+      tpl := i.getTiploc( l.Tiploc )
+      if tpl != nil && tpl.Station {
+        hr := l.Times.Time.Get() / 3600
+        i.getSlot( tpl.CRS, hr )[ key ] = nil
+      }
+    }
+
+  }
   return nil
 }
--- timetable/update.go
+++ timetable/update.go
@@ -8,7 +8,11 @@ import (
 
 type index struct {
   cif *cif.CIF
-  index map[string]map[int][]string
+  bucket *bolt.Bucket
+  // Index of schedules keyed by crs/hour
+  index map[string]map[int]map[string]interface{}
+  // index of tiplocs, used to determine if we want to know about an entry
+  tpl map[string]*cif.Tiploc
 }
 
 // Update updates the timetable
@@ -17,7 +21,12 @@ func ( t * Timetable ) Update( c *cif.CIF, bucket *bolt.Bucket ) error {
   log.Println( "Updating timetable" )
 
   // Index of crs/hour/schedules
-  index := &index{ cif: c, index: make( map[string]map[int][]string ) }
+  index := &index{
+    cif: c,
+    bucket: bucket,
+    index: make( map[string]map[int]map[string]interface{} ),
+    tpl: make( map[string]*cif.Tiploc ),
+  }
 
   if err := bucket.ForEach( index.process ); err != nil {
     log.Println( "Timetable update failed", err )
@@ -28,16 +37,31 @@ func ( t * Timetable ) Update( c *cif.CIF, bucket *bolt.Bucket ) error {
   return nil
 }
 
-func (i *index) getSlot( c string, h int ) []string {
+func (i *index) getSlot( c string, h int ) map[string]interface{} {
   m, exists := i.index[ c ]
   if !exists {
-    m := make( map[int][]string )
+    m = make( map[int]map[string]interface{} )
     i.index[ c ] = m
   }
 
-  return m[ h ]
+  s, exists := m[ h ]
+  if !exists {
+    s = make( map[string]interface{} )
+    m[ h ] = s
+  }
+
+  return s
 }
 
-func (i *index ) setSlot( c string, h int, s []string ) {
-  i.index[ c ][ h ] = s
+func (i *index) getTiploc( tpl string ) *cif.Tiploc {
+  if t, exists := i.tpl[ tpl ]; exists {
+    return t
+  }
+
+  if t, exists := i.cif.GetTiploc( i.bucket.Tx(), tpl ); exists {
+    i.tpl[ tpl ] = t
+    return t
+  }
+
+  return nil
 }
